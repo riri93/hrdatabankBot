8883e5a90b6d7ce2bc00a9ae93cd9c8d63d6cd85
riri93 <rihabkallel93@gmail.com>
2017-08-17 17:06:50 +0200

    Update EchoApplication.java

--- src/main/java/com/example/demo/EchoApplication.java
+++ src/main/java/com/example/demo/EchoApplication.java
@@ -9,6 +9,11 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+import ai.api.AIConfiguration;
+import ai.api.AIDataService;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
@@ -19,7 +24,38 @@ public class EchoApplication {
 	@EventMapping
 	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
 		System.out.println("event: " + event);
-		return new TextMessage(event.getMessage().getText() + "rihab");
+		/*
+		 * if (event.getMessage().getText() == "Hi" ||
+		 * event.getMessage().getText().equals("Hi")) { return new
+		 * TextMessage("Hello, How are you?"); } else { return new
+		 * TextMessage(event.getMessage().getText()); }
+		 */
+		AIConfiguration configuration = new AIConfiguration("4082b96e0b0d4665938c945ba58c2ef5");
+
+		AIDataService dataService = new AIDataService(configuration);
+
+		try {
+			AIRequest request = new AIRequest(event.getMessage().getText());
+
+			AIResponse response = dataService.request(request);
+
+			if (response.getStatus().getCode() == 200) {
+				System.out.println(response.getResult().getFulfillment().getDisplayText());
+			} else {
+				System.err.println(response.getStatus().getErrorDetails());
+			}
+
+			if (response.getResult().getAction() == "weather") {
+				System.out.println(event.getMessage().getText() + ":weather");
+				return new TextMessage(event.getMessage().getText() + ":weather");
+			} else {
+				System.err.println(event.getMessage().getText() + ":NOT-weather");
+				return new TextMessage(event.getMessage().getText() + ":NOT-weather");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
 	}
 
 	@EventMapping
